0f1cc88b24aade8fdb76c383e5d9fbf788528012
CheezItMan <chris@adadevelopersacademy.org>
2018-04-03 15:41:20 -0700

    solution works

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -4,6 +4,27 @@
 # e.g. 1st fibonacci number is 1
 # ....
 # e.g. 6th fibonacci number is 8
+
+
 def fibonacci(n)
-  return 2
+  raise ArgumentError if n.class != Integer
+  raise ArgumentError if n < 0
+  return 0 if n < 1
+  if n < 2
+    return 1
+  end
+
+  one_ago = 1
+  two_ago = 1
+  current = 2
+  current_val = 2
+
+  while current < n
+    current += 1
+    current_val = one_ago + two_ago
+    two_ago = one_ago
+    one_ago = current_val
+  end
+
+  return current_val
 end
